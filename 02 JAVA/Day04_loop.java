9f7bf7de59b1d4133546ff76d354e5a4ca07b496
saebuck <folwofqer@gmail.com>
2017-07-13 16:39:32 +0900

    자바 4일차 반복문

---
+++ 02 JAVA/Day04_loop.java
@@ -0,0 +1,54 @@
+package first.second;
+
+public class loop {
+
+	public static void main(String[] args) {
+		System.out.println("»ï°¢Çü1");
+		for(int i=0; i<10; i++) {
+			for(int j=0; j<10; j++) {
+				if(i>=j) System.out.print("*");
+				else System.out.print(" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println("»ï°¢Çü2");
+		for(int i=9; i>=0; i--) {
+			for(int j=0; j<10; j++) {
+				if(i>=j) System.out.print("*");
+				else System.out.print(" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println("X");
+		for(int i=0; i<10; i++) {
+			for(int j=0; j<10; j++) {
+				if(i==j || i+j==9) System.out.print("*");
+				else System.out.print(" ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		System.out.println("¸¶¸§¸ð");
+		for(int i=0; i<10; i++) {
+			for(int j=0; j<10; j++) {
+				if(i<5) {
+					if(j+i==4||j-i==5) System.out.print("*");
+					else System.out.print(" ");
+				}
+				else {
+					if(i-j==5||j+i==14) System.out.print("*");
+					else System.out.print(" ");					
+				}
+			}
+			System.out.println();
+		}
+		
+		
+
+	}
+
+}
+
